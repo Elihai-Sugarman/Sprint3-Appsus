3175b44e8b198667cf01546894d53b273f86527f
Elihai Sugarman <elihaisugarman@gmail.com>
2022-11-09 12:46:15 +0200

    connected mail and keep to routes

---
+++ apps/mail/pages/email-app.cmp.js
@@ -0,0 +1,7 @@
+export default {
+    template: `
+        <footer class="email-app">
+            <h1>Email App</h1>
+        </footer>
+    `,
+}
--- routes.js
+++ routes.js
@@ -1,20 +1,30 @@
 import homePage from './views/app-home.cmp.js'
 import aboutPage from './views/app-about.cmp.js'
+import emailApp from './apps/mail/pages/email-app.cmp.js'
+import keepApp from './apps/keep/pages/keep-app.cmp.js'
 
 const { createRouter, createWebHashHistory } = VueRouter
 
 const routerOptions = {
-	history: createWebHashHistory(),
-	routes: [
-		{
-			path: '/',
-			component: homePage,
-		},
-		{
-			path: '/about',
-			component: aboutPage,
-		},
-	],
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/',
+            component: homePage,
+        },
+        {
+            path: '/about',
+            component: aboutPage,
+        },
+        {
+            path: '/mail',
+            component: emailApp,
+        },
+        {
+            path: '/keep',
+            component: keepApp,
+        },
+    ],
 }
 
 export const router = createRouter(routerOptions)
