6186f2bf387a6b6e7694b5041c0518c169f61fcc
beeri <amitohayon232@gmail.com>
2022-11-09 13:18:51 +0200

    new file:   assets/img/keep.png 	modified:   assets/style/cmps/app-footer.css 	modified:   assets/style/cmps/app-header.css 	modified:   assets/style/cmps/home-page.css 	modified:   views/app-home.cmp.js

--- views/app-home.cmp.js
+++ views/app-home.cmp.js
@@ -1,7 +1,11 @@
 export default {
 	template: `
-        <section class="home-page">
-            <h1>Home sweet home</h1>
+        <section class="home-page flex">
+        <section class="home-nav">
+        <button class="keep"></button>
+        <button class="mail"></button>
+        <button class="book"></button>
+        </section>
         </section>
     `,
 }
